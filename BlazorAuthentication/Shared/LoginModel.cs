3325f115dd30dc47cad37c9761d8b212270d20ac
adriana634 <50183872+adriana634@users.noreply.github.com>
2022-09-14 21:27:41 +0200

    Add the login controller

---
+++ BlazorAuthentication/Shared/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAuthentication.Shared
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Email { get; set; } = default!;
+
+        [Required]
+        public string Password { get; set; } = default!;
+
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ BlazorAuthentication/Shared/LoginResult.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorAuthentication.Shared
+{
+    public class LoginResult
+    {
+        public bool Successful { get; set; }
+        public string Error { get; set; } = default!;
+        public string Token { get; set; } = default!;
+    }
+}
